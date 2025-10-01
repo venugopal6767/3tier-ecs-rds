51f7582ee5701e033351a8d8bd20ea311b767492
venu67776 <83317608+venu67776@users.noreply.github.com>
2025-10-01 19:29:58 +0530

    new

--- application-code/app-tier/DbConfig.js
+++
@@ -1,6 +0,0 @@
-module.exports = Object.freeze({
-    DB_HOST     : process.env.DB_HOST,       // RDS endpoint
-    DB_USER     : process.env.DB_USER,       // username
-    DB_PWD      : process.env.DB_PASSWORD,   // password
-    DB_DATABASE : process.env.DB_NAME        // database name
-});
